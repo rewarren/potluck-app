35883a6ea0ba98a0be0b42ca1946f6459c094c88
Rachel Warren <raelwa@gmail.com>
2016-04-07 09:43:06 -0400

    modularized services, updated GET request to reach out to the API router

---
+++ public/scripts/services/event.js
@@ -0,0 +1,14 @@
+angular.module('potluckApp')
+.service('eventService', function($http){
+
+  this.getEventDetails = function(callback){
+    $http.get('/api/eventdetails')
+    .then(callback)
+  }
+
+  this.getFoods = function(callback){
+    $http.get('/api/foods')
+    .then(callback)
+  }
+
+})
